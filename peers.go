c11e8c4609669290851784d49f4b4ef507741b5f
EmptyCity-111 <3047874865@qq.com>
2025-07-13 21:27:10 +0800

    节点选择器

--- peers.go
+++ peers.go
@@ -1,6 +1,18 @@
 package Orbit_Cache
 
-import "context"
+import (
+	"Orbit-Cache/consistenthash"
+	"Orbit-Cache/grpc"
+	"Orbit-Cache/registry"
+	"context"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"sync"
+	"time"
+)
+
+const defaultSvcName = "Orbit-Cache"
 
 // PeerPicker 的 PickPeer() 方法用于根据传入的 key 选择相应节点 Peer
 type PeerPicker interface {
@@ -8,10 +20,196 @@ type PeerPicker interface {
 	Close() error
 }
 
-// Peer Get方法用于从对应 group 查找缓存值 PeerGetter 对应HTTP 客户端
+// Peer 缓存节点接口 Get方法用于从对应 group 查找缓存值 Peer 对应HTTP 客户端
 type Peer interface {
 	Get(group string, key string) ([]byte, error)
-	set(ctx context.Context, group string, key string, value []byte) error
+	Set(ctx context.Context, group string, key string, value []byte) error
 	Delete(group string, key string) (bool, error)
 	Close() error
+}
+
+// ClientPicker 实现了PeerPicker接口
+type ClientPicker struct {
+	selfAddr string
+	svcName  string
+	mu       sync.RWMutex
+	consHash *consistenthash.Map
+	clients  map[string]*grpc.Client
+	etcdCli  *clientv3.Client
+	ctx      context.Context
+	cancel   context.CancelFunc
+}
+
+type PickerOption func(*ClientPicker)
+
+// WithServiceName 设置服务名称
+func WithServiceName(svcName string) PickerOption {
+	return func(c *ClientPicker) {
+		c.svcName = svcName
+	}
+}
+
+// NewClientPicker 创建新的ClientPicker实例
+func NewClientPicker(addr string, opts ...PickerOption) (*ClientPicker, error) {
+	ctx, cancel := context.WithCancel(context.Background())
+	picker := &ClientPicker{
+		selfAddr: addr,
+		svcName:  defaultSvcName,
+		clients:  make(map[string]*grpc.Client),
+		consHash: consistenthash.New(),
+		ctx:      ctx,
+		cancel:   cancel,
+	}
+
+	for _, opt := range opts {
+		opt(picker)
+	}
+
+	client, err := clientv3.New(clientv3.Config{
+		Endpoints:   registry.DefaultConfig.Endpoints,
+		DialTimeout: registry.DefaultConfig.DialTimeout,
+	})
+	if err != nil {
+		cancel()
+		return nil, fmt.Errorf("failed to create etcd client: %w", err)
+	}
+	picker.etcdCli = client
+
+	//启动服务发现
+	//if err := picker.
+	return picker, nil
+}
+
+// startServiceDiscovery 启动服务发现
+func (p *ClientPicker) startServiceDiscovery() error {
+	// 先进行全量更新
+	if err := p.fetchAllServices(); err != nil {
+		return err
+	}
+
+	// 启动增量更新
+	go p.watchServiceChanges()
+	return nil
+
+}
+
+// watchServiceChanges 监听服务实例变化
+func (p *ClientPicker) watchServiceChanges() {
+	watcher := clientv3.NewWatcher(p.etcdCli)
+	watchChan := watcher.Watch(p.ctx, "/service/"+p.svcName, clientv3.WithPrefix())
+
+	for {
+		select {
+		//接收取消信号
+		case <-p.ctx.Done():
+			watcher.Close()
+			return
+		case resp := <-watchChan:
+			p.handleWatchEvents(resp.Events)
+		}
+	}
+}
+
+// handleWatchEvents 处理监听到的事件
+func (p *ClientPicker) handleWatchEvents(events []*clientv3.Event) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	for _, event := range events {
+		addr := string(event.Kv.Value)
+		if addr == p.selfAddr {
+			continue
+		}
+
+		switch event.Type {
+		case clientv3.EventTypePut:
+			if _, exists := p.clients[addr]; !exists {
+				p.set(addr)
+				logrus.Infof("New service discovered at %s", addr)
+			}
+		case clientv3.EventTypeDelete:
+			if client, exists := p.clients[addr]; exists {
+				client.Close()
+				p.remove(addr)
+				logrus.Infof("Service removed at %s", addr)
+			}
+		}
+	}
+}
+
+// fetchAllServices 获取所有服务实例
+func (p *ClientPicker) fetchAllServices() error {
+	ctx, cancel := context.WithTimeout(p.ctx, 3*time.Second)
+	defer cancel()
+
+	resp, err := p.etcdCli.Get(ctx, "/services/"+p.svcName, clientv3.WithPrefix())
+	if err != nil {
+		return fmt.Errorf("failed to get all services: %v", err)
+	}
+
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	for _, kv := range resp.Kvs {
+		addr := string(kv.Value)
+		if addr != "" && addr != p.selfAddr {
+			p.set(addr)
+			logrus.Infof("Discovered service at %s", addr)
+		}
+	}
+	return nil
+}
+
+// set 添加服务实例
+func (p *ClientPicker) set(addr string) {
+	if client, err := grpc.NewClient(addr, p.svcName, p.etcdCli); err == nil {
+		//利用一致性hash添加服务实例
+		p.consHash.Add(addr)
+		p.clients[addr] = client
+		logrus.Infof("Successfully created client for %s", addr)
+	} else {
+		logrus.Errorf("Failed to create client for %s: %v", addr, err)
+	}
+}
+
+// remove 移除服务实例
+func (p *ClientPicker) remove(addr string) {
+	p.consHash.Remove(addr)
+	delete(p.clients, addr)
+}
+
+// PickPeer 选择peer节点
+func (p *ClientPicker) PickPeer(key string) (Peer, bool, bool) {
+	p.mu.RLock()
+	defer p.mu.RUnlock()
+
+	if addr := p.consHash.Get(key); addr != "" {
+		if client, exists := p.clients[addr]; exists {
+			return client, true, addr == p.selfAddr
+		}
+	}
+	return nil, false, false
+}
+
+// Close 关闭所有资源
+func (p *ClientPicker) Close() error {
+	//发送关闭信号
+	p.cancel()
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	var errs []error
+	for addr, client := range p.clients {
+		if err := client.Close(); err != nil {
+			errs = append(errs, fmt.Errorf("failed to close client for %s: %w", addr, err))
+		}
+	}
+
+	if err := p.etcdCli.Close(); err != nil {
+		errs = append(errs, fmt.Errorf("failed to close etcd client for %s: %w", p.svcName, err))
+	}
+	if len(errs) > 0 {
+		return fmt.Errorf("errors while closing: %v", errs)
+	}
+	return nil
 }
